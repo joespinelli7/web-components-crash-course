b54f8faa143162da4a5c06ef087c8d94a5a32abf
Joe Spinelli <josephspinelli@yahoo.com>
2022-05-10 14:36:04 -0500

    Attaching shadow dom to custom element

--- userCard.js
+++ userCard.js
@@ -1,9 +1,16 @@
 class UserCard extends HTMLElement {
+  // this keyword used to reference our custom element and what is attached to it
   constructor() {
     // super() methods calls on the constructor of the HTMLElement class we're extending
     super();
+
+    // attaching shadow dom to this custom element
+    this.attachShadow({ mode: 'open'});
+    // cloneNode: returns a copy of the node, if deep is true also returns node's descendants
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+
     // this keyword used to reference our custom element
-    this.innerHTML = `${this.getAttribute('name')}`;
+    this.innerHTML = ``;
   }
 }
 
