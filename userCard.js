38a2502725be344f7d7ad32357053b3a0e16dbb9
Joe Spinelli <josephspinelli@yahoo.com>
2022-05-10 14:42:38 -0500

    Adding template to custom element and using shadow dom to manipulate

--- userCard.js
+++ userCard.js
@@ -1,3 +1,15 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    h3 {
+      color: coral;
+    }
+  </style>
+  <div class="user-card">
+    <h3></h3>
+  </div>
+`
+
 class UserCard extends HTMLElement {
   // this keyword used to reference our custom element and what is attached to it
   constructor() {
@@ -8,9 +20,8 @@ class UserCard extends HTMLElement {
     this.attachShadow({ mode: 'open'});
     // cloneNode: returns a copy of the node, if deep is true also returns node's descendants
     this.shadowRoot.appendChild(template.content.cloneNode(true))
-
-    // this keyword used to reference our custom element
-    this.innerHTML = ``;
+    // use shadowRoot to select and manipulate stuff from our custom element
+    this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
   }
 }
 
