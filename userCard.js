473ac3343e53a6b5aadcf5d65112e6632c7147e9
Joe Spinelli <josephspinelli@yahoo.com>
2022-05-10 14:53:57 -0500

    Finish template/css and add dynamic img to template

--- userCard.js
+++ userCard.js
@@ -1,12 +1,40 @@
 const template = document.createElement('template');
 template.innerHTML = `
   <style>
-    h3 {
-      color: coral;
-    }
+  .user-card {
+    font-family: 'Arial', sans-serif;
+    background: #f4f4f4;
+    width: 500px;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-gap: 10px;
+    margin-bottom: 15px;
+    border-bottom: darkorchid 5px solid;
+   }
+
+  .user-card img {
+    width:: 100%;
+  }
+
+  .user-card button {
+    cursor: pointer;
+    background: darkorchid;
+    color: #fff;
+    border: 0;
+    border-radius: 5px;
+    padding: 5px 10px;
+  }
   </style>
   <div class="user-card">
-    <h3></h3>
+    <img />
+    <div>
+      <h3></h3>
+      <div class="info">
+        <p>Email:</p>
+        <p>Phone:</p>
+      </div>
+      <button id="toggle-info">Hide Info</button>
+    </div>
   </div>
 `
 
@@ -22,6 +50,7 @@ class UserCard extends HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true))
     // use shadowRoot to select and manipulate stuff from our custom element
     this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+    this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
   }
 }
 
