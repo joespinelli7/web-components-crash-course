2095d5f34b8ddab8f576ec51f7e3bf0bc61d4a11
Joe Spinelli <josephspinelli@yahoo.com>
2022-05-10 14:28:34 -0500

    Passing attribute name value for custom component

--- userCard.js
+++ userCard.js
@@ -2,5 +2,9 @@ class UserCard extends HTMLElement {
   constructor() {
     // super() methods calls on the constructor of the HTMLElement class we're extending
     super();
+    // this keyword used to reference our custom element
+    this.innerHTML = `${this.getAttribute('name')}`;
   }
 }
+
+window.customElements.define('user-card', UserCard)
